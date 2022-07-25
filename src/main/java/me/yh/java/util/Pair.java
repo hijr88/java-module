7c935d700a03ad18163ce99395bb3b81c0f57013
yohan <hirj88@naver.com>
2022-07-25 17:22:10 +0900

    Pair Class

---
+++ src/main/java/me/yh/java/util/Pair.java
@@ -0,0 +1,17 @@
+package me.yh.java.util;
+
+public class Pair<K, V>{
+    private final K k;
+    private final V v;
+
+    public Pair(K k, V v) {
+        this.k = k;
+        this.v = v;
+    }
+    public K getKey() {
+        return k;
+    }
+    public V getValue() {
+        return v;
+    }
+}
